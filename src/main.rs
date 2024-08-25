5982844f98ec9c4c7dfe5a44cd014caf3e562393
Skypex <84551201+Skyppex@users.noreply.github.com>
2024-08-25 18:51:12 +0200

    add k arg

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,18 @@ use itertools::Itertools;
 use std::io::{self, BufRead, Write};
 
 fn main() {
+    let args = std::env::args().collect::<Vec<String>>();
+
     let stdin = io::stdin();
     let lines: Vec<String> = stdin.lock().lines().filter_map(Result::ok).collect();
-    let permutations = lines.iter().permutations(lines.len());
+
+    let k = args
+        .get(1)
+        .unwrap_or(&lines.len().to_string())
+        .parse::<usize>()
+        .unwrap_or(lines.len());
+
+    let permutations = lines.iter().permutations(k);
     let stdout = io::stdout();
     let mut handle = stdout.lock();
 
