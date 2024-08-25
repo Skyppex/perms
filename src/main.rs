6495f5e811c263bc39a42e9b14ab12fd273a969a
Skypex <84551201+Skyppex@users.noreply.github.com>
2024-08-25 18:40:59 +0200

    implemented algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use itertools::Itertools;
+use std::io::{self, BufRead, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    let lines: Vec<String> = stdin.lock().lines().filter_map(Result::ok).collect();
+    let permutations = lines.iter().permutations(lines.len());
+    let stdout = io::stdout();
+    let mut handle = stdout.lock();
+
+    for perm in permutations {
+        match writeln!(handle, "{}", perm.iter().join(";")) {
+            Ok(_) => (),
+            Err(_) => break,
+        }
+    }
 }
